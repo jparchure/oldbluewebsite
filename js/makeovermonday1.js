1c5f2f0881b873e99371873f71646a6091c16de8
Jay Parchure <jparchure@users.noreply.github.com>
2017-01-08 11:16:51 -0600

    Delete makeovermonday1.js

--- js/makeovermonday1.js
+++
@@ -1,5 +0,0 @@
-var divElement = document.getElementById('viz1483894320435');                    
-var vizElement = divElement.getElementsByTagName('object')[0];                    
-if ( divElement.offsetWidth > 800 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*1.77)+'px';}
-var scriptElement = document.createElement('script');
-scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';vizElement.parentNode.insertBefore(scriptElement, vizElement);
